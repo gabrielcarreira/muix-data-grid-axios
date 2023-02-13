322048d9675904afe1c98c6a5bc2ef1c57ec9244
Gabriel Carreira <gabriel.carreira1@gmail.com>
2023-02-13 14:10:18 -0300

    modo de edição

--- src/DataGrid.js
+++ src/DataGrid.js
@@ -1,31 +1,62 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Container, Typography } from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid'
+import { Edit } from '@mui/icons-material'
 import api from './services/api'
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'first_name',
-    headerName: 'Nome',
-    width: 150,
-    editable: true
-  },
-  {
-    field: 'last_name',
-    headerName: 'Sobrenome',
-    width: 150,
-    editable: true
-  },
-  {
-    field: 'email',
-    headerName: 'Email',
-    width: 200,
-    editable: true
-  }
-]
-
 export default function DataGridDemo() {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'first_name',
+      headerName: 'Nome',
+      width: 200,
+      editable: true
+    },
+    {
+      field: 'last_name',
+      headerName: 'Sobrenome',
+      width: 200,
+      editable: true
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 300,
+      editable: true
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      width: 80,
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<Edit />}
+          label="Edit"
+          onClick={editUser(params)}
+        />
+      ]
+    }
+  ]
+
+  const editUser = React.useCallback(
+    (params) => () => {
+      const data = {
+        first_name: params.row.first_name,
+        last_name: params.row.last_name,
+        email: params.row.email
+      }
+
+      api
+        .put(`/client/${params.row.id}`, data)
+        .then((response) => alert(response))
+        .catch((err) => {
+          console.error('Ocorreu um erro' + err)
+        })
+    },
+    []
+  )
+
   const [clients, getClients] = useState({ isLoading: true, data: null })
 
   useEffect(() => {
@@ -60,7 +91,7 @@ export default function DataGridDemo() {
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
-          experimentalFeatures={{ newEditingApi: false }}
+          experimentalFeatures={{ newEditingApi: true }}
         />
       </Box>
     </Container>
